3f9ee7a8b892206ca956ded839553159d9183e1d
Brandon <76419269+BrandonLee28@users.noreply.github.com>
2023-06-10 13:26:52 -0400

    3

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template, Response
 from student import StudentTerminal
-import barcodescanner
+import cv2
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/dashboard')
 def index():
     terminal = StudentTerminal("students.db")
-    
-    return render_template("index.html")
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return render_template("dashboard.html")
+
+@app.route('/')
+def signin():
+
+    return render_template("signin.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
